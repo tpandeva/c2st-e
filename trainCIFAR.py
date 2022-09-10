201fa7c9e41fa634fb0658ddf803da1a62e063cf
Teodora Pandeva <t.p.pandeva@uva.nl>
2022-09-10 08:34:25 +0200

    cifar c2st

--- trainCIFAR.py
+++ trainCIFAR.py
@@ -51,48 +51,51 @@ def train(cfg: DictConfig):
         data_trans[i] = TT(d0)
     data_new = data_trans
     del data_trans
+    for s in range(10):
+        pl.seed_everything(s)
+        ind = np.random.choice(len(data_old), len(data_new),replace=False)
+        data_old = data_old[ind, :,:,:]
+        if 1:
+            data = torch.utils.data.TensorDataset(data_old,data_new)
+            train_data, val_data, test_data =torch.utils.data.random_split(data, [1000, 200, 821])
+        x = torch.concat((data_old,data_new)).float()
+        y = torch.concat((torch.ones(int(data_old.shape[0])), torch.zeros(int(data_new.shape[0]))))
 
-    ind = np.random.choice(len(data_old), len(data_new),replace=False)
-    data_old = data_old[ind, :,:,:]
-    if 1:
-        data = torch.utils.data.TensorDataset(data_old,data_new)
-        train_data, val_data, test_data =torch.utils.data.random_split(data, [1000, 200, 821])
-    x = torch.concat((data_old,data_new)).float()
-    y = torch.concat((torch.ones(int(data_old.shape[0])), torch.zeros(int(data_new.shape[0]))))
+        data = torch.utils.data.TensorDataset(x, y)
+        train_data, val_data, test_data = torch.utils.data.random_split(data, [2000, 400, 1642])
 
-    data = torch.utils.data.TensorDataset(x, y)
-    train_data, val_data, test_data = torch.utils.data.random_split(data, [2000, 400, 1642])
-
-    logger.info(OmegaConf.to_yaml(cfg))
-
-    pl.seed_everything(cfg.seed)
-
-            # Initialize the network
-    callbacks: List[Callback] = hydra.utils.instantiate(cfg.early_stopping)
-    classifier = Discriminator(cfg.model)#Featurizer(cfg.model)#MMD_DClassifier(cfg.model)
-
-            # Train
-
-    train_loader = torch.utils.data.DataLoader(train_data, batch_size=cfg.data.batch_size, shuffle=True)
-    val_loader = torch.utils.data.DataLoader(val_data, batch_size=cfg.data.batch_size)
-    trainer = pl.Trainer(**cfg.trainer, accelerator="gpu",devices=1, callbacks=callbacks)
-    trainer.fit(classifier, train_loader, val_loader)
+        logger.info(OmegaConf.to_yaml(cfg))
 
 
-            # Initialize the network
-    resp, rese, resl, resm, resme, resscf = [], [], [], [], [], []
+
+                # Initialize the network
+        callbacks: List[Callback] = hydra.utils.instantiate(cfg.early_stopping)
+        classifier = Discriminator(cfg.model)#Featurizer(cfg.model)#MMD_DClassifier(cfg.model)
+
+                # Train
+
+        train_loader = torch.utils.data.DataLoader(train_data, batch_size=cfg.data.batch_size, shuffle=True)
+        val_loader = torch.utils.data.DataLoader(val_data, batch_size=cfg.data.batch_size)
+        trainer = pl.Trainer(**cfg.trainer, accelerator="gpu",devices=1, callbacks=callbacks)
+        trainer.fit(classifier, train_loader, val_loader)
 
 
-    test_loader = torch.utils.data.DataLoader(test_data, batch_size = 821)
+                # Initialize the network
+        resp, rese, resl, resm, resme, resscf = [], [], [], [], [], []
 
-    stats = trainer.test(classifier, test_loader)
-    resp.append(stats[0]['test_c2stp'])
-    rese.append(stats[0]['test_c2ste'])
-    resl.append(stats[0]['test_c2stl_c2stl'] > stats[0]['test_c2stl_thres'])
 
-   # resm.append(stats[0]['MMD-D P'])
-    del classifier, trainer
+        test_loader = torch.utils.data.DataLoader(test_data, batch_size = 821)
 
+        stats = trainer.test(classifier, test_loader)
+        resp.append(stats[0]['test_c2stp'])
+        rese.append(stats[0]['test_c2ste'])
+        resl.append(stats[0]['test_c2stl_c2stl'] > stats[0]['test_c2stl_thres'])
+
+       # resm.append(stats[0]['MMD-D P'])
+        del classifier, trainer
+    print(resp)
+    print(rese)
+    print(resl)
 
 
 

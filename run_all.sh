f5346340de3a0dcab8e6b8155cda9f6dcd7a707f
Timsey <tim_bb@hotmail.com>
2022-10-07 17:48:00 +0200

    bash script

---
+++ run_all.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+conda activate c2st
+
+# Base experiments
+python trainMRI.py --num_dataset_samples 100 --num_partitions 0 --num_epochs 30 --do_early_stopping True \
+     --dataset_sizes 200 400 1000 2000 3000 4000 5000 --settings 1a 1b 2 --seed None \
+     --save_dir /home/timsey/Projects/c2st-e/results/mri/base_exp_oct7
+
+# Meta-analysis experiments
+for i in {1..100}; do
+    python trainMRI.py --num_dataset_samples 0 --num_partitions 3 --num_epochs 30 --do_early_stopping True \
+     --dataset_sizes 200 400 1000 2000 --settings 1a 1b 2 --seed None \
+     --save_dir /home/timsey/Projects/c2st-e/results/mri/meta_analysis_oct7
+done

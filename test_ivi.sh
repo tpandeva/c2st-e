7953fb8383518ef47a32c2860d90c32e6009c119
Timsey <tim_bb@hotmail.com>
2022-10-10 13:48:32 +0200

    ivi test

--- test_ivi.sh
+++ test_ivi.sh
@@ -30,7 +30,7 @@ echo "export PYTHONPATH=:\$PYTHONPATH:" >> ${SLURM}
 {
     python /home/tbbakke/anaconda3/envs/c2st/bin/python ${LOGS_DIR}/trainMRI_all.py \
         --num_dataset_samples 1 --num_partitions 0 --num_epochs 30 --do_early_stopping True \
-        --dataset_sizes 3 --settings 1a 1b 2 --seed None \
+        --dataset_sizes 1000 --settings 1a 1b 2 --seed None \
         --save_dir /home/tbbakke/c2st-e/results/mri/tests
 } >> ${SLURM}
 

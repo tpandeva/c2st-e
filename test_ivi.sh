51c8859a1c72c6f55ddc6a82ffc5a834315db752
Timsey <tim_bb@hotmail.com>
2022-10-10 15:01:41 +0200

    ivi test

--- test_ivi.sh
+++ test_ivi.sh
@@ -2,7 +2,7 @@
 
 #conda update -n base -c defaults conda
 
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH://home/tbbakke/anaconda3/envs/c2st/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH://home/tbbakke/anaconda3/envs/c2st/lib
 
 # Original code folder is here
 MAIN_DIR=/home/tbbakke/c2st-e

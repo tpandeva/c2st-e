806ad8e3814fe3fa50f54059b46c349201db0c3c
Timsey <tim_bb@hotmail.com>
2022-10-11 13:32:18 +0200

    run ivi base

--- run_all_ivi.sh
+++ run_all_ivi.sh
@@ -79,7 +79,7 @@ echo "export PYTHONPATH=:\$PYTHONPATH:" >> ${SLURM}
 {
     echo CUDA_VISIBLE_DEVICES=0 /home/tbbakke/anaconda3/envs/c2st/bin/python ${LOGS_DIR}/c2st-e/trainMRI_all.py \
         --num_dataset_samples 100 --num_partitions 0 --num_epochs 30 --do_early_stopping True \
-        --dataset_sizes 200 --settings 1a 1b 2 --seed None \
+        --dataset_sizes 400 --settings 1a 1b 2 --seed None \
         --data_dir /home/tbbakke/data/fastMRI/singlecoil/singlecoil_all \
         --pathology_path /home/tbbakke/fastmri-plus/Annotations/knee.csv \
         --checked_path /home/tbbakke/fastmri-plus/Annotations/knee_file_list.csv \
@@ -91,176 +91,39 @@ sbatch ${SLURM}
 sleep 1
 
 
-## Create dir for specific experiment run
-#dt=$(date '+%F_%T.%3N')
-#LOGS_DIR=${LAUNCH_DIR}/${dt}
-#mkdir -p "${LOGS_DIR}"
-## Copy code to experiment folder
-#rsync -arm ${MAIN_DIR} --stats --exclude-from=${MAIN_DIR}/"SYNC_EXCLUDE" ${LOGS_DIR};
-#JOB_NAME=base_400_800
-#SLURM=${LOGS_DIR}/run.slrm
-## Make SLURM file
-#echo "${SLURM}"
-#echo "#!/bin/bash" > ${SLURM}
-#echo "#SBATCH --job-name=$JOB_NAME" >> ${SLURM}
-#echo "#SBATCH --output=${LOGS_DIR}/%j.out" >> ${SLURM}
-#echo "#SBATCH --error=${LOGS_DIR}/%j.err" >> ${SLURM}
-#echo "#SBATCH --gres=gpu:1" >> ${SLURM}
-#echo "#SBATCH --cpus-per-task=12" >> ${SLURM}
-#echo "#SBATCH --ntasks-per-node=1" >> ${SLURM}
-#echo "#SBATCH --mem=8G" >> ${SLURM}
-#echo "#SBATCH --time=7-0:00:00" >> ${SLURM}
-#echo "#SBATCH --nodes=1" >> ${SLURM}
-#echo "export PYTHONPATH=:\$PYTHONPATH:" >> ${SLURM}
-#
-#{
-#    echo CUDA_VISIBLE_DEVICES=0 /home/tbbakke/anaconda3/envs/c2st/bin/python ${LOGS_DIR}/c2st-e/trainMRI_all.py \
-#        --num_dataset_samples 100 --num_partitions 0 --num_epochs 30 --do_early_stopping True \
-#        --dataset_sizes 400 --settings 1a 1b 2 --seed None \
-#        --data_dir /home/tbbakke/data/fastMRI/singlecoil/singlecoil_all \
-#        --pathology_path /home/tbbakke/fastmri-plus/Annotations/knee.csv \
-#        --checked_path /home/tbbakke/fastmri-plus/Annotations/knee_file_list.csv \
-#        --save_dir /home/tbbakke/c2st-e/results/mri/base_exp_oct10
-#    echo
-#
-#    echo CUDA_VISIBLE_DEVICES=0 /home/tbbakke/anaconda3/envs/c2st/bin/python ${LOGS_DIR}/c2st-e/trainMRI_all.py \
-#        --num_dataset_samples 100 --num_partitions 0 --num_epochs 30 --do_early_stopping True \
-#        --dataset_sizes 800 --settings 1a 1b 2 --seed None \
-#        --data_dir /home/tbbakke/data/fastMRI/singlecoil/singlecoil_all \
-#        --pathology_path /home/tbbakke/fastmri-plus/Annotations/knee.csv \
-#        --checked_path /home/tbbakke/fastmri-plus/Annotations/knee_file_list.csv \
-#        --save_dir /home/tbbakke/c2st-e/results/mri/base_exp_oct10
-#    echo
-#} >> ${SLURM}
-#
-#sbatch ${SLURM}
-#sleep 1
+# Create dir for specific experiment run
+dt=$(date '+%F_%T.%3N')
+LOGS_DIR=${LAUNCH_DIR}/${dt}
+mkdir -p "${LOGS_DIR}"
+# Copy code to experiment folder
+rsync -arm ${MAIN_DIR} --stats --exclude-from=${MAIN_DIR}/"SYNC_EXCLUDE" ${LOGS_DIR};
+JOB_NAME=base_400_800
+SLURM=${LOGS_DIR}/run.slrm
+# Make SLURM file
+echo "${SLURM}"
+echo "#!/bin/bash" > ${SLURM}
+echo "#SBATCH --job-name=$JOB_NAME" >> ${SLURM}
+echo "#SBATCH --output=${LOGS_DIR}/%j.out" >> ${SLURM}
+echo "#SBATCH --error=${LOGS_DIR}/%j.err" >> ${SLURM}
+echo "#SBATCH --gres=gpu:1" >> ${SLURM}
+echo "#SBATCH --cpus-per-task=12" >> ${SLURM}
+echo "#SBATCH --ntasks-per-node=1" >> ${SLURM}
+echo "#SBATCH --mem=8G" >> ${SLURM}
+echo "#SBATCH --time=7-0:00:00" >> ${SLURM}
+echo "#SBATCH --nodes=1" >> ${SLURM}
+echo "export PYTHONPATH=:\$PYTHONPATH:" >> ${SLURM}
 
+{
 
+    echo CUDA_VISIBLE_DEVICES=0 /home/tbbakke/anaconda3/envs/c2st/bin/python ${LOGS_DIR}/c2st-e/trainMRI_all.py \
+        --num_dataset_samples 100 --num_partitions 0 --num_epochs 30 --do_early_stopping True \
+        --dataset_sizes 800 --settings 1a 1b 2 --seed None \
+        --data_dir /home/tbbakke/data/fastMRI/singlecoil/singlecoil_all \
+        --pathology_path /home/tbbakke/fastmri-plus/Annotations/knee.csv \
+        --checked_path /home/tbbakke/fastmri-plus/Annotations/knee_file_list.csv \
+        --save_dir /home/tbbakke/c2st-e/results/mri/base_exp_oct10
+    echo
+} >> ${SLURM}
 
-## Create dir for specific experiment run
-#dt=$(date '+%F_%T.%3N')
-#LOGS_DIR=${LAUNCH_DIR}/${dt}
-#mkdir -p "${LOGS_DIR}"
-## Copy code to experiment folder
-#rsync -arm ${MAIN_DIR} --stats --exclude-from=${MAIN_DIR}/"SYNC_EXCLUDE" ${LOGS_DIR};
-#JOB_NAME=base_1000_2000
-#SLURM=${LOGS_DIR}/run.slrm
-## Make SLURM file
-#echo "${SLURM}"
-#echo "#!/bin/bash" > ${SLURM}
-#echo "#SBATCH --job-name=$JOB_NAME" >> ${SLURM}
-#echo "#SBATCH --output=${LOGS_DIR}/%j.out" >> ${SLURM}
-#echo "#SBATCH --error=${LOGS_DIR}/%j.err" >> ${SLURM}
-#echo "#SBATCH --gres=gpu:1" >> ${SLURM}
-#echo "#SBATCH --cpus-per-task=12" >> ${SLURM}
-#echo "#SBATCH --ntasks-per-node=1" >> ${SLURM}
-#echo "#SBATCH --mem=8G" >> ${SLURM}
-#echo "#SBATCH --time=7-0:00:00" >> ${SLURM}
-#echo "#SBATCH --nodes=1" >> ${SLURM}
-#echo "export PYTHONPATH=:\$PYTHONPATH:" >> ${SLURM}
-#{
-#    echo CUDA_VISIBLE_DEVICES=0 /home/tbbakke/anaconda3/envs/c2st/bin/python ${LOGS_DIR}/c2st-e/trainMRI_all.py \
-#        --num_dataset_samples 100 --num_partitions 0 --num_epochs 30 --do_early_stopping True \
-#        --dataset_sizes 1000 --settings 1a 1b 2 --seed None \
-#        --data_dir /home/tbbakke/data/fastMRI/singlecoil/singlecoil_all \
-#        --pathology_path /home/tbbakke/fastmri-plus/Annotations/knee.csv \
-#        --checked_path /home/tbbakke/fastmri-plus/Annotations/knee_file_list.csv \
-#        --save_dir /home/tbbakke/c2st-e/results/mri/base_exp_oct10
-#    echo
-#
-#    echo CUDA_VISIBLE_DEVICES=0 /home/tbbakke/anaconda3/envs/c2st/bin/python ${LOGS_DIR}/c2st-e/trainMRI_all.py \
-#        --num_dataset_samples 100 --num_partitions 0 --num_epochs 30 --do_early_stopping True \
-#        --dataset_sizes 2000 --settings 1a 1b 2 --seed None \
-#        --data_dir /home/tbbakke/data/fastMRI/singlecoil/singlecoil_all \
-#        --pathology_path /home/tbbakke/fastmri-plus/Annotations/knee.csv \
-#        --checked_path /home/tbbakke/fastmri-plus/Annotations/knee_file_list.csv \
-#        --save_dir /home/tbbakke/c2st-e/results/mri/base_exp_oct10
-#    echo
-#} >> ${SLURM}
-#
-#sbatch ${SLURM}
-#sleep 1
-#
-#
-#
-## Create dir for specific experiment run
-#dt=$(date '+%F_%T.%3N')
-#LOGS_DIR=${LAUNCH_DIR}/${dt}
-#mkdir -p "${LOGS_DIR}"
-## Copy code to experiment folder
-#rsync -arm ${MAIN_DIR} --stats --exclude-from=${MAIN_DIR}/"SYNC_EXCLUDE" ${LOGS_DIR};
-#JOB_NAME=base_3000_4000
-#SLURM=${LOGS_DIR}/run.slrm
-## Make SLURM file
-#echo "${SLURM}"
-#echo "#!/bin/bash" > ${SLURM}
-#echo "#SBATCH --job-name=$JOB_NAME" >> ${SLURM}
-#echo "#SBATCH --output=${LOGS_DIR}/%j.out" >> ${SLURM}
-#echo "#SBATCH --error=${LOGS_DIR}/%j.err" >> ${SLURM}
-#echo "#SBATCH --gres=gpu:1" >> ${SLURM}
-#echo "#SBATCH --cpus-per-task=12" >> ${SLURM}
-#echo "#SBATCH --ntasks-per-node=1" >> ${SLURM}
-#echo "#SBATCH --mem=8G" >> ${SLURM}
-#echo "#SBATCH --time=7-0:00:00" >> ${SLURM}
-#echo "#SBATCH --nodes=1" >> ${SLURM}
-#echo "export PYTHONPATH=:\$PYTHONPATH:" >> ${SLURM}
-#{
-#    echo CUDA_VISIBLE_DEVICES=0 /home/tbbakke/anaconda3/envs/c2st/bin/python ${LOGS_DIR}/c2st-e/trainMRI_all.py \
-#        --num_dataset_samples 100 --num_partitions 0 --num_epochs 30 --do_early_stopping True \
-#        --dataset_sizes 3000 --settings 1a 1b 2 --seed None \
-#        --data_dir /home/tbbakke/data/fastMRI/singlecoil/singlecoil_all \
-#        --pathology_path /home/tbbakke/fastmri-plus/Annotations/knee.csv \
-#        --checked_path /home/tbbakke/fastmri-plus/Annotations/knee_file_list.csv \
-#        --save_dir /home/tbbakke/c2st-e/results/mri/base_exp_oct10
-#    echo
-#
-#    echo CUDA_VISIBLE_DEVICES=0 /home/tbbakke/anaconda3/envs/c2st/bin/python ${LOGS_DIR}/c2st-e/trainMRI_all.py \
-#        --num_dataset_samples 100 --num_partitions 0 --num_epochs 30 --do_early_stopping True \
-#        --dataset_sizes 4000 --settings 1a 1b 2 --seed None \
-#        --data_dir /home/tbbakke/data/fastMRI/singlecoil/singlecoil_all \
-#        --pathology_path /home/tbbakke/fastmri-plus/Annotations/knee.csv \
-#        --checked_path /home/tbbakke/fastmri-plus/Annotations/knee_file_list.csv \
-#        --save_dir /home/tbbakke/c2st-e/results/mri/base_exp_oct10
-#    echo
-#} >> ${SLURM}
-#
-#sbatch ${SLURM}
-#sleep 1
-#
-#
-#
-## Create dir for specific experiment run
-#dt=$(date '+%F_%T.%3N')
-#LOGS_DIR=${LAUNCH_DIR}/${dt}
-#mkdir -p "${LOGS_DIR}"
-## Copy code to experiment folder
-#rsync -arm ${MAIN_DIR} --stats --exclude-from=${MAIN_DIR}/"SYNC_EXCLUDE" ${LOGS_DIR};
-#JOB_NAME=base_5000
-#SLURM=${LOGS_DIR}/run.slrm
-## Make SLURM file
-#echo "${SLURM}"
-#echo "#!/bin/bash" > ${SLURM}
-#echo "#SBATCH --job-name=$JOB_NAME" >> ${SLURM}
-#echo "#SBATCH --output=${LOGS_DIR}/%j.out" >> ${SLURM}
-#echo "#SBATCH --error=${LOGS_DIR}/%j.err" >> ${SLURM}
-#echo "#SBATCH --gres=gpu:1" >> ${SLURM}
-#echo "#SBATCH --cpus-per-task=12" >> ${SLURM}
-#echo "#SBATCH --ntasks-per-node=1" >> ${SLURM}
-#echo "#SBATCH --mem=8G" >> ${SLURM}
-#echo "#SBATCH --time=7-0:00:00" >> ${SLURM}
-#echo "#SBATCH --nodes=1" >> ${SLURM}
-#echo "export PYTHONPATH=:\$PYTHONPATH:" >> ${SLURM}
-#{
-#    echo CUDA_VISIBLE_DEVICES=0 /home/tbbakke/anaconda3/envs/c2st/bin/python ${LOGS_DIR}/c2st-e/trainMRI_all.py \
-#        --num_dataset_samples 100 --num_partitions 0 --num_epochs 30 --do_early_stopping True \
-#        --dataset_sizes 5000 --settings 1a 1b 2 --seed None \
-#        --data_dir /home/tbbakke/data/fastMRI/singlecoil/singlecoil_all \
-#        --pathology_path /home/tbbakke/fastmri-plus/Annotations/knee.csv \
-#        --checked_path /home/tbbakke/fastmri-plus/Annotations/knee_file_list.csv \
-#        --save_dir /home/tbbakke/c2st-e/results/mri/base_exp_oct10
-#    echo
-#} >> ${SLURM}
-#
-#sbatch ${SLURM}
-#sleep 1
+sbatch ${SLURM}
+sleep 1
